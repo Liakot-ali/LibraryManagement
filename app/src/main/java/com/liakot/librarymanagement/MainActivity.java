59e0ad5fec5d7675933132d5733cb91d1ab34625
Liakot Ali Liton <liakotali2k18@gmail.com>
2021-08-28 20:10:22 +0600

    Category Design Complete

--- app/src/main/java/com/liakot/librarymanagement/MainActivity.java
+++ app/src/main/java/com/liakot/librarymanagement/MainActivity.java
@@ -2,13 +2,87 @@ package com.liakot.librarymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    CircleImageView homeProfilePicture;
+
+    ImageView homeSearchBook, homeContacts, homeCategory, homeMyBooks, homeNextBook, homePendingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeProfilePicture = findViewById(R.id.homeProfilePicture);
+        homeSearchBook = findViewById(R.id.homeSearchBook);
+        homeCategory = findViewById(R.id.homeCategory);
+        homeContacts = findViewById(R.id.homeContacts);
+        homeMyBooks = findViewById(R.id.homeMyBooks);
+        homePendingList = findViewById(R.id.homePendingList);
+        homeNextBook = findViewById(R.id.homeNextBook);
+
+
+        homeSearchBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homeCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homeContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homeMyBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homePendingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PendingListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homeNextBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NextBookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        homeProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivityAdmin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/liakot/librarymanagement/NextBookActivity.java
@@ -0,0 +1,14 @@
+package com.liakot.librarymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_book);
+    }
+}
